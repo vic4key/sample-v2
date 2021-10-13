e6ad8dfef6a1b66b6e03dae58355189bb64cab88
Vic P <vic4key@gmail.com>
2021-10-14 03:02:21 +0700

    Initial

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 06, 2019 at 07:17 PM
+-- Generation Time: Oct 13, 2021 at 08:01 PM
 -- Server version: 5.6.37
 -- PHP Version: 5.6.32
 
@@ -34,17 +34,18 @@ CREATE TABLE IF NOT EXISTS `tbl_user` (
   `email` varchar(255) NOT NULL,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
-  `age` int(10) unsigned NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `age` int(10) unsigned DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_user`
 --
 
 INSERT INTO `tbl_user` (`id`, `social`, `user`, `pass`, `email`, `first_name`, `last_name`, `age`) VALUES
-(1, '', 'vic', '123', 'vic@sample.email', 'Vic', 'V.', 11),
-(2, '', 'test', '123', 'test@sample.email', 'Test', 'T.', 22),
-(3, '', 'user', '123', 'user@sample.email', 'User', 'U.', 33);
+(1, '', 'vic', '202cb962ac59075b964b07152d234b70', 'vic@sample.email', 'Vic', 'P.', 11),
+(2, '', 'test', '202cb962ac59075b964b07152d234b70', 'test@sample.email', 'Test', 'T.', 22),
+(3, '', 'user', '202cb962ac59075b964b07152d234b70', 'user@sample.email', 'User', 'U.', 33),
+(4, 'GitHub', 'vic4key', '', 'vic4key@gmail.com', 'Vic', 'P.', NULL);
 
 --
 -- Indexes for dumped tables
@@ -64,7 +65,7 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
